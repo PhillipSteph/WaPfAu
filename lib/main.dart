e1dcc318c15e9c88323560e9ee2ce7d038efdaf2
Phillip <phillipstephan314@gmail.com>
2025-09-01 13:35:26 +0200

    name

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Wapfau_test',
+      title: 'Wapfau',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
