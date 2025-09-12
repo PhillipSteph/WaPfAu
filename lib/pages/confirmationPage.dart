572d665c918e927d6d7042e092b61eee92d6651b
Phillip Stephan <Phillip.Stephan@deutschebahn.com>
2025-09-12 08:39:45 +0200

    created file for page

---
+++ lib/pages/confirmationPage.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:wapfau/models/user.dart';
+import 'package:wapfau/services/coreService.dart';
+
+import '../models/course.dart';
+import '../services/courseService.dart';
+import '../widgets/card.dart';
+
+
+class MyConfirmationPage extends StatefulWidget {
+  const MyConfirmationPage({super.key});
+  @override
+  State<MyConfirmationPage> createState() => _MyConfirmationPageState();
+}
+
+class _MyConfirmationPageState extends State<MyConfirmationPage> {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:
+      Container(),
+    );
+  }
+}
